166b639132577115ad412a824d910cc790066b30
lgoldstein <lgoldstein@groupon.com>
2015-12-17 23:59:15 -0800

    cleanup of print statements

--- lib/hash_diff.rb
+++ lib/hash_diff.rb
@@ -1,6 +1,5 @@
 class Hash
   def diff(other)
-    p "DIFF"
     dup.
       delete_if do |k, v|
       compare_equal v, other[k]
@@ -10,20 +9,14 @@ class Hash
 
   private
   def compare_equal(a, b)
-    p 1
     if a.kind_of? BigDecimal or b.kind_of? BigDecimal
-      p 2
       if a.nil? or b.nil?
-        p 3
         false
       else
-        p 4
         a.to_d == b.to_d
       end
     else
-      p 5
       a == b
     end
-    # false
   end
 end
--- spec/hash_diff_spec.rb
+++ spec/hash_diff_spec.rb
@@ -11,11 +11,9 @@ describe 'Hash' do
   let(:double10) { 10.to_d }
   let(:double20) { 20.to_d }
   it "??" do
-    precision = 5
     plain = [nil, double10, double10, double20, 1].combination(2).map {|a, b|
       {a => b}.diff({b => a})
     }
-    p plain
 
     with = [{nil=>double10, double10=>nil}, {nil=>double10, double10=>nil}, {nil=>double20, double20=>nil}, {nil=>1, 1=>nil}, {}, {double10=>double20, double20=>double10}, {double10=>1, 1=>double10}, {double10=>double20, double20=>double10}, {double10=>1, 1=>double10}, {double20=>1, 1=>double20}]
     expect(plain).to eq with
